ca12d0bb5a798d05bdbb36e7251c60e71fef90d5
vegas007gof <vegas007gof@mail.ru>
2022-11-26 01:43:06 +0300

    исправил
    добавил шибанг

--- pyProj/zadanie.py
+++ pyProj/zadanie.py
@@ -1,10 +1,14 @@
-m = (1, 2, 3, 4, 5, 6, 78, 8, 4, 5, 64, 3, 2, 34)
-n = []
-for i in range(len(m)):
-    if (i + 1) % 2 != 0:
-        n.append(i*m[i])
-    else:
-        n.append(m[i]/i)
-n = tuple(n)
-print(type(n))
-print(n)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    m = (1, 2, 3, 4, 5, 6, 78, 8, 4, 5, 64, 3, 2, 34)
+    n = []
+    for i in range(len(m)):
+        if (i+1) % 2 != 0:
+            n.append(i*m[i])
+        else:
+            n.append(m[i]/i)
+    n = tuple(n)
+    print(type(n))
+    print(n)

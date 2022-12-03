4bb1002c4840f190575790a5b92f628fff32739a
vegas007gof <vegas007gof@mail.ru>
2022-12-03 09:53:23 +0300

    изменения в индивидуальном

--- pyProj/zadanie.py
+++ pyProj/zadanie.py
@@ -5,10 +5,12 @@ if __name__ == '__main__':
     m = (1, 2, 3, 4, 5, 6, 78, 8, 4, 5, 64, 3, 2, 34)
     n = []
     for i in range(len(m)):
-        if (i+1) % 2 != 0:
+        if i != 0 or i % 2 != 0:
             n.append(i*m[i])
         else:
-            n.append(m[i]/i)
-    n = tuple(n)
-    print(type(n))
+            if i:
+                 n.append(m[i]/i)
+            else:
+                n.append(0)
+    n = m[0:len(m)]
     print(n)
